b225777a57a2db684f1e50392d76561d2d7f3d6d
Alan Jenkins <alan.james.jenkins@gmail.com>
2022-03-20 12:49:21 +0000

    Fix variable name in update-repo.sh

--- .github/actions/update-repos/update-repo.sh
+++ .github/actions/update-repos/update-repo.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 REPO_NAME="$1"
 
-cd "$REPO_DIR"
-echo "Updating repo $REPO_DIR"
+cd "$REPO_NAME"
+echo "Updating repo $REPO_NAME"
 find . -name '*.pkg.tar.*' -exec "echo "Adding package {} repo "${REPO_NAME}" && repo-add -n "./${REPO_NAME}.db" {}\;
